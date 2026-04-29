09158c85f492aa44dd29e0cf4e802d5436ef42dd
P4bl0p0l0 <poldenegri@gmail.com>
2026-04-29 10:14:54 -0600

    Delete comments from 02_insert_sample_data.sql
    Remove unused comments regarding sample data.

--- sql/02_insert_sample_data.sql
+++ sql/02_insert_sample_data.sql
@@ -1,9 +1,1 @@
-/*
-This file is intentionally not used by the Python demo.
 
-Sample data is maintained in:
-
-data/earnings_core_sample.csv
-
-The Python script loads the CSV directly into the SQLite table.
-*/
