27543b0ba8df4decad03797e7d5edaa36934a408
P4bl0p0l0 <poldenegri@gmail.com>
2026-04-29 09:45:28 -0600

    Create earnings_core table with income details

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,1 +1,21 @@
+DROP TABLE IF EXISTS earnings_core;
 
+CREATE TABLE earnings_core (
+    earner_uuid TEXT,
+    period TEXT,
+    vehicle_category TEXT,
+
+    gross_monthly_income REAL,
+    service_income REAL,
+    tips_amount REAL,
+    referral_bonuses REAL,
+    promotional_incentives REAL,
+    cash_collections_adjustment REAL,
+
+    iva_withheld REAL,
+    isr_withheld REAL,
+
+    payout_date TEXT,
+    service_period_start TEXT,
+    service_period_end TEXT
+);
